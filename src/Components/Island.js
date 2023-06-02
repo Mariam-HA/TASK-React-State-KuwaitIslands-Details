a82da4a8cdf3cf70494aa61aa3fec9f539b34be6
AMAL ALRASDAN <mariam.alrashdan97@gmail.com>
2023-06-03 01:35:14 +0300

    task kuwait islands completed

--- src/Components/Island.js
+++ src/Components/Island.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-export default function Island({ island }) {
+export default function Island({ island, setIsland }) {
+  // const handle = (event) => {
+  //   console.log("//////");
+  //   setIsland(island);
+  // };
   return (
-    <div className="Island">
+    <div className="Island" onClick={() => setIsland(island)}>
       <h3>{island.name}</h3>
       {/* YOU HAVE TO CHANGE THE VISITORS COUNT */}
       <p>Visitors 0</p>
--- src/Components/IslandForm.js
+++ src/Components/IslandForm.js
@@ -1,12 +1,41 @@
+import { useState } from "react";
+
 export default function IslandForm({ island }) {
+  // use 2 state for the name and phone
+
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  function handleName(event) {
+    setName(event.target.value);
+  }
+
+  const handlePhone = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handleBookNow = () => {
+    window.confirm(
+      `Are you sure you want to book to ${island.name} with the Name:${name},Phone:${phone} `
+    );
+  };
+
   return (
     <div className="form">
       <h2>{island.name}</h2>
       <img src={island.img} alt={island.name} />
       <h3>Book a trip to {island.name} island</h3>
-      <input placeholder="Type Full Name" />
-      <input placeholder="Type Phone Number" />
-      <button className="book" onClick={() => {}}>
+      <input placeholder="Type Full Name" onChange={handleName} />
+      <input
+        placeholder="Type Phone Number"
+        // type="number"
+        type="tel"
+        id="phone"
+        name="phone"
+        pattern="[0-9]{8}"
+        onChange={handlePhone}
+      />
+      <button className="book" onClick={handleBookNow}>
         Book for today!
       </button>
     </div>
